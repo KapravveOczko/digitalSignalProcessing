d836788a3a2dedaf8038a727f12384424618a4ae
thekrzeslo <mateuszgrzeszczak17@gmail.com>
2024-03-16 23:51:41 +0100

    refactor: Added functions to avoid duplications.

--- custom_notebook.py
+++ custom_notebook.py
@@ -29,7 +29,6 @@ class CustomNotebook(ttk.Notebook):
         self.create_operatin_on_signals_widgets()
         self.create_generate_signals_widgets()
 
-
     def create_operatin_on_signals_widgets(self):
         self.operation_frame = ttk.Frame(self)
         self.add(self.operation_frame, text="Operacje na sygnałach")
@@ -42,8 +41,7 @@ class CustomNotebook(ttk.Notebook):
         operation_types_keys = list(OPERATION_TYPES.keys())
         operation_types_values = list(OPERATION_TYPES.values())
 
-        operation_label = ttk.Label(operation_frame, text="Wybierz Operacje")
-        operation_label.grid(column=0, row=row_number)
+        operation_label = self.create_label(operation_frame, "Wybierz Operacje", row_number)
         operation = tk.StringVar()
         operation_option_menu = ttk.OptionMenu(operation_frame, operation, operation_types_values[0], *operation_types_values)
         operation_option_menu.grid(column=1, row=row_number)
@@ -51,15 +49,13 @@ class CustomNotebook(ttk.Notebook):
         row_number += 1
         self.tab_names = [self.tab(tab_id, "text") for tab_id in self.tabs()[2::]]
 
-        first_tab_label = ttk.Label(operation_frame, text="Pierwszy Sygnał")
-        first_tab_label.grid(column=0, row=row_number)
+        first_tab_label = self.create_label(operation_frame, "Pierwszy Sygnał", row_number)
         self.first_tab = tk.StringVar(value="")
         self.first_tab_menu = ttk.Combobox(operation_frame, textvariable=self.first_tab, values=[])
         self.first_tab_menu.grid(column=1, row=row_number)
 
         row_number += 1
-        second_tab_label = ttk.Label(operation_frame, text="Drugi Sygnał")
-        second_tab_label.grid(column=0, row=row_number)
+        second_tab_label = self.create_label(operation_frame, "Drugi Sygnał", row_number)
         self.second_tab = tk.StringVar(value="")
         self.second_tab_menu = ttk.Combobox(operation_frame, textvariable=self.second_tab, values=[])
         self.second_tab_menu.grid(column=1, row=row_number)
@@ -127,8 +123,7 @@ class CustomNotebook(ttk.Notebook):
             'frequency': "Częstotliwość",
         }
 
-        signal_type_label = ttk.Label(signal_params_frame, text="Typ sygnału:")
-        signal_type_label.grid(row=row_number, column=0, sticky="w")
+        signal_type_label = self.create_label(signal_params_frame, "Typ sygnału:", row_number)
 
         signal_types_keys = list(SIGNAL_TYPES.keys())
         signal_types_values = list(SIGNAL_TYPES.values())
@@ -143,8 +138,7 @@ class CustomNotebook(ttk.Notebook):
             self.render_input(signal_params_frame, label_text_dict[key], row_number, params_val_dict[key])
 
         row_number += 1
-        hist_bins_label = ttk.Label(signal_params_frame, text="Przedział histogramu:")
-        hist_bins_label.grid(row=row_number, column=0, sticky="w")
+        hist_bins_label = self.create_label(signal_params_frame, "Przedział histogramu:", row_number)
 
         hist_bins_menu = ttk.Combobox(signal_params_frame, textvariable=params_val_dict['hist_bins'], values=[
             5, 10, 15, 20
@@ -187,11 +181,18 @@ class CustomNotebook(ttk.Notebook):
             self.update_tab_list()
 
     def render_input(self, signal_params_frame, label_text, row_number, default_value):
-        label = ttk.Label(signal_params_frame, text=label_text)
-        label.grid(row=row_number, column=0, sticky="w")
+        self.create_label(signal_params_frame, label_text, row_number)
+        self.create_entry(signal_params_frame, default_value, row_number)
 
-        entry = ttk.Entry(signal_params_frame, textvariable=default_value)
+    def create_label(self, paren, label_text, row_number):
+        label = ttk.Label(paren, text=label_text)
+        label.grid(row=row_number, column=0, sticky="w")
+        return label
+
+    def create_entry(self, parent, default_value, row_number):
+        entry = ttk.Entry(parent, textvariable=default_value)
         entry.grid(row=row_number, column=1, padx=10, sticky="w")
+        return entry
 
     def update_tab_list(self):
         self.tab_names = [self.tab(tab_id, "text") for tab_id in self.tabs()][2::]
@@ -200,10 +201,13 @@ class CustomNotebook(ttk.Notebook):
         self.second_tab_menu['values'] = self.tab_names
 
         if self.tab_names:
-            self.first_tab_menu.set(self.tab_names[0])
-            self.second_tab_menu.set(self.tab_names[0])
-            self.first_tab.set(self.tab_names[0])
-            self.second_tab.set(self.tab_names[0])
+            self.set_tab_values(self.tab_names[0])
+
+    def set_tab_values(self, value):
+        self.first_tab_menu.set(value)
+        self.second_tab_menu.set(value)
+        self.first_tab.set(value)
+        self.second_tab.set(value)
 
     def on_close_press(self, event):
         """Called when the button is pressed over the close button"""
@@ -211,7 +215,7 @@ class CustomNotebook(ttk.Notebook):
         element = self.identify(event.x, event.y)
 
         if "close" in element:
-            index = self.index("@%d,%d" % (event.x, event.y))
+            index = self.index(f"@{event.x},{event.y}")
             self.state(['pressed'])
             self._active = index
             return "break"
